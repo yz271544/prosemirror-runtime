2536fcd4bb45bbca2d4fca7b84cc09a71c1220b8
huzhengyang <Lyndon.Hu9@gmail.com>
2024-08-19 18:35:20 +0800

    init

---
+++ src/server1.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = 3000
+
+const getSum = (limit: number) => {
+    let sum = 0;
+    for (let i = 0; i < limit; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+
+app.get("/", (req, res) => {
+    const result = getSum(1500);
+    res.setHeader('Cache-Control', 'no-store')
+    res.send(`Processed function getSum on main thread and result: ${result}`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
